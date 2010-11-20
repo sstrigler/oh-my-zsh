51d69498cca985d7a53dd0a2f2df1bdd9ab2ec7a
Christopher Loessl <cloessl@x-berg.de>
2010-11-20 14:35:12 +0100

    my custom file

---
+++ custom/zzzhashier.zsh
@@ -0,0 +1,9 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+setopt rm_star_wait
+bindkey '\e.' insert-last-word
+REPORTTIME=3
+TIMEFMT="Real: %E User: %U System: %S Percent: %P Cmd: %J"
