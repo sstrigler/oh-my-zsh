56d0e229e45e500dcb6fe933b8ef224dd44e89f7
Christopher Loessl <cloessl@x-berg.de>
2010-12-25 02:43:01 +0100

    aliases added

---
+++ custom/aliases.zsh
@@ -0,0 +1,37 @@
+# espeak with reasonable speed
+alias espeak='espeak -s 100'
+
+# fullscreen, please
+alias feh='feh -F'
+
+alias dpms='xset dpms force suspend'
+
+# spawn new terminal in current place
+alias c="urxvtc"
+
+# Global aliases for often used commands in the command line.
+alias -g E='2>&1'
+alias -g L='E | less'
+alias -g D='E | colordiff L'
+alias -g G='| grep'
+alias -g S='| sort'
+alias -g U='| uniq'
+alias -g H='| head'
+alias -g T='| tail'
+
+# Make going up directories simple.
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+
+alias vimsyn="vim --servername lasync"
+
+# dropbox
+alias db="python ~/.dropbox/dropbox.py"
+
+# hivemind
+alias t="todo.pl"
+
+alias sortseries="tvnamer -c ~/.tvnamer.json ~/space/unsorted"
+
+alias wcd=wicd-curses
